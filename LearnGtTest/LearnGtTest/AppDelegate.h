b42a6c77f053bf92dcf70ed5b465271c36ae96b1
甄冠卿 <zhenguanqing@126.com>
2019-08-15 14:51:04 +0800

    test

---
+++ LearnGtTest/LearnGtTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnGtTest
+//
+//  Created by Zhen.GQ on 2019/8/15.
+//  Copyright © 2019 ZGQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnGtTest/LearnGtTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LearnGtTest
+//
+//  Created by Zhen.GQ on 2019/8/15.
+//  Copyright © 2019 ZGQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
